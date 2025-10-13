8fe4d2110b1966e94e64a5cde743da844169cdd9
deelaka-ransilu <ransilu.deelaka@gmail.com>
2025-10-13 22:37:40 +0530

    added gender collection

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../Layout/Hero";
+import GenderCollectionSection from "../Products/GenderCollectionSection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <GenderCollectionSection />
     </div>
   );
 };
